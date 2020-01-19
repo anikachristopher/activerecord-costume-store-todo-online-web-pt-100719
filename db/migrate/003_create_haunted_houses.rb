33ceeea6a4daa4d12448a387e71511099e37323c
Anika Christopher <anika.mchristopher@gmail.com>
2020-01-19 23:30:08 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,16 @@
 # Create your haunted_houses migration here
+class HauntedHouses < ActiveRecord::Migration[5.2]
+   def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      
+      t.integer :costume_inventory
+      t.integer :num_of_employees
+      t.boolean :still_open_for_business
+      t.datetime :opening_time
+      t.datetime :closing_time
+    end
+  end
+end
